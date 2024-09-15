7e17299e677f49c90fc964b5be838d124a4a39c5
Jack Freedman <jf96@email.sc.edu>
2024-09-15 19:48:34 -0400

    A

--- exercises/06_is_intro/script.js
+++ exercises/06_is_intro/script.js
@@ -1,8 +1,12 @@
 // clicker counter
 let count=0
 
-document.getElementById("count").addEventListener("click"), ()=>{
+document.getElementById("count").addEventListener("click", ()=>{
     count++;
     document.getElementById("counter").innerHTML=count;
-};
+});
 
+//Random picture Gen
+document.getElementById("picture").addEventListener("click",()=>{
+    window.location.reload();
+});
