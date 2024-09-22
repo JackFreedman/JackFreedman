45c00b7e849e8bcd31a59be17c4b6c9b943cda54
Jack Freedman <jf96@email.sc.edu>
2024-09-22 18:48:58 -0400

    A

--- exercises/Java_conditionals/script.js
+++ exercises/Java_conditionals/script.js
@@ -24,7 +24,7 @@ exercise2Link.addEventListener("click", (e) => {
 function showExercise(exercise){
     document.getElementById('colors').style.display = 'none';
     document.getElementById('pictures').style.display = 'none';
-    document.getElementById(exercise).style.display = 'block';
+    document.getElementById(exercises).style.display = 'block';
 }
 
 
