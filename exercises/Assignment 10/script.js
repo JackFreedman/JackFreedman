8a4193cb2508bd448af9ad3680b36a0e2d4f1f45
Jack Freedman <jf96@email.sc.edu>
2024-10-02 11:19:11 -0400

    A

---
+++ exercises/Assignment 10/script.js
@@ -0,0 +1,8 @@
+const images=[];
+images["images/read.jpg"]="reading";
+images["images/clown.jpg"]="clowning";
+images["images/birthday.jpg"]="birthday!";
+images["images/shovel.jpg"]="digging!";
+images["images/rain.jpg"]="raining!";
+images["images/work.jpg"]="studying!";
+
