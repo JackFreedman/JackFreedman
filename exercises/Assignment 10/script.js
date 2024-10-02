b3f6fae25de9da988db203e84b617ba09f906539
Jack Freedman <jf96@email.sc.edu>
2024-10-02 11:27:32 -0400

    A

--- exercises/Assignment 10/script.js
+++ exercises/Assignment 10/script.js
@@ -6,3 +6,10 @@ images["images/shovel.jpg"]="digging!";
 images["images/rain.jpg"]="raining!";
 images["images/work.jpg"]="studying!";
 
+const div = document.getElementById(image-list);
+
+for(let image in images){
+    let myImage =document.createElement("img");
+    myImage.src=image;
+    div.append(myImage);
+}
