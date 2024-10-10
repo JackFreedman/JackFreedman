b82735940dc31b72be519bd7bbfb6e137253a01d
Jack Freedman <jf96@email.sc.edu>
2024-10-10 15:13:00 -0400

    A

--- exercises/java_loops/script.js
+++ exercises/java_loops/script.js
@@ -1,21 +1,24 @@
-/*HERE we are gonna use JS to take whatever they insert use it as a variable to insert that number of stars into the box */
-/*Then we make it so each time we click the button for draw it draws 10 stars BUT the stars that were placed before have to be made hidden/removed first */
-/* make it so the stars randomly are placed within the borders of however the box is */
-/*Start number ALWAYS = 0 */
 document.getElementById("draw-stars").onclick = () => {
-    const startNum = 0;
-    const endNum = parseInt(document.getElementById("txt-star-count").value);
-    const ul = document.getElementById("numbers");
-  
-    console.log(`counting from ${startNum} to ${endNum}`);
-  
-    if(endNum >= startNum){
-      for(let i = startNum; i <= endNum; i++){
-        ul.append(createLi(i));
-      }
-    } else {
-      for(let i = endNum; i >= startNum; i--){
-        ul.append(createLi(i));
-      }
+    const blackBox = document.getElementById('black-box');
+    const starCount = parseInt(document.getElementById('txt-star-count').value);
+    const DrawStar =document.getElementById('star');
+    blackBox.innerHTML = ' ';
+
+    
+
+    for(let i=0; i < starCount; i++){
+        const Stars = document.createElement('p');
+        Stars.classList.add('star');
+        Stars.style.top = Math.random() * (blackBox.clientHeight - 50) + 'px';
+        Stars.style.left = Math.random() * (blackBox.clientWidth - 50) + 'px';
+        Stars.setAttribute('data-star-number', i + 1);
+        blackBox.append(Stars);
+        
     }
-  }
+    
+}
+  
+  function Stars(count) {
+    blackBox;
+}
+
